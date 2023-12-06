30ac43c08313afe5c47f5ce2cde49e016969413e
Jérémy Barbe <jeremy.barbe.zion@gmail.com>
2023-12-06 23:12:10 +0100

    fix(gatsby): add metadata url

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: 'Barbe Jérémy',
     description: 'Barbe Jeremy is a fullstack developer, specialized in react.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://main--nimble-brigadeiros-fbfa8a.netlify.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
